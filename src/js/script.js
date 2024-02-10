eeae0cda9516c610056304ff4fe56bd0b8c2bf81
Sergej <serik777pazan@gmail.com>
2024-02-10 22:43:33 +0100

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// a = 15;
+// console.log(a);
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+  a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+console.log(name);
+var name = 'Ivan';
+
+{
+
+var result = 50;
+
+}
+
+console.log(result);
+
+// 1-Aufgabe
+const userName = "John";
+let userNumber = 25;
+userNumber = 24;
+
+// 2-Aufgabe
+const storeName = 'My awesome store';
+
+const storeDescription = {
+    budget: 10000,
+    employees: ['Alex', 'John', 'Sam'],
+    products: {
+        'soap': 20,
+        'water': 5
+    },
+    open: true
+}
